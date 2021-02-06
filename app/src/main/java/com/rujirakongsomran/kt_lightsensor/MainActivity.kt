17e37580831215e3196f658811e1ccdb94904e77
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-02-06 14:59:25 +0700

    Finish

--- app/src/main/java/com/rujirakongsomran/kt_lightsensor/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_lightsensor/MainActivity.kt
@@ -1,11 +1,66 @@
 package com.rujirakongsomran.kt_lightsensor
 
-import androidx.appcompat.app.AppCompatActivity
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.rujirakongsomran.kt_lightsensor.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var sensorManager: SensorManager
+    private var brightness: Sensor? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        setupSensorStuff()
+
+    }
+
+    private fun setupSensorStuff() {
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        brightness = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+    }
+
+    private fun brightness(brightness: Float): String {
+        return when (brightness.toInt()) {
+            0 -> "Pitch black"
+            in 1..10 -> "Dark"
+            in 11..50 -> "Grey"
+            in 51..5000 -> "Normal"
+            in 5001..25000 -> "Incredibly bright"
+            else -> "This light will blind you"
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_LIGHT) {
+            val light = event.values[0]
+            binding.tv.text = "Sensor: $light\n${brightness(light)}"
+            binding.cpb.setProgressWithAnimation(light)
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        return
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, brightness, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
     }
 }
